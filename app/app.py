369740defd0b00ab770572132adb1b19c8654c39
MPappia <elliot@MBP-de-Elliot.lan>
2023-12-15 16:20:25 +0100

    Ajout REV + templates + routes

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import render_template
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route("/")
+def accueil():
+    return render_template("pages/accueil.html")
 @app.route("/home")
 def home():
     return "Hello World !"
+
+#REV-2
+@app.route("/division/<int:numerateur>/<int:denominateur>", methods=['GET'])
+def division(numerateur, denominateur) : 
+    resultat = numerateur/denominateur
+    return str(resultat)
+
+#REV-3
